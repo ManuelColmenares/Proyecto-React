20ced19e3fa64b6cd0e2860a54869962b5b87059
ManuelColmenares <colmenaresmanueljt@gmail.com>
2022-09-08 00:24:06 -0300

    Modificación del item.jsx y ItemList.jsx

--- web-app/src/components/Item.jsx
+++ web-app/src/components/Item.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
-const Item = ({ nombre, img, precio }) => {
+const Item = ({ nombre, descripcion, imagen, precio }) => {
 	return (
 		<div className="card">
-			<img src={img} className="card-img-top" alt={nombre} />
+			<img src={imagen} className="card-img-top" alt={nombre} />
 			<div className="card-body">
 				<h5 className="card-title text-center">
 					<b>{nombre}</b>
 				</h5>
+				<p className="card-text text-center">{descripcion}</p>
 				<p className="card-text text-center">${precio}</p>
 			</div>
 		</div>
--- web-app/src/components/ItemList.jsx
+++ web-app/src/components/ItemList.jsx
@@ -6,7 +6,7 @@ const ItemList = ({ items }) => {
 		<div className="row">
 			{items.map((item) => (
 				<div key={item.id} className="col-md-4-py-3">
-					<Item nombre={item.nombre} imagen={item.img} precio={item.precio} />
+					<Item nombre={item.nombre} descripcion={item.desc} imagen={item.img} precio={item.precio} />
 				</div>
 			))}
 		</div>
