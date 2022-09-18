65aa839dd075df986768ebad272afc5e174e1ae4
Manuel <manuelcolmenaresunerg@gmail.com>
2022-09-18 15:22:52 -0300

    Se creo nuevo componente productos dentro del carpeta mock y se modifico el ItemListcontainer traernos los productos desde el nuevo componente

--- web-app/src/components/ItemListContainer.jsx
+++ web-app/src/components/ItemListContainer.jsx
@@ -1,6 +1,5 @@
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
+import Products from "./mock/Products"
 import ItemList from "./ItemList";
 
 
@@ -8,43 +7,9 @@ const ItemListContainer = (props) => {
 	const [items, setItems] = useState([]);
 
 	useEffect(() => {
-		const productos = [
-			{
-				id: 1,
-				nombre: "Tres Leche",
-				desc: "Bizcochuelo húmedo en tres leches (leche condensada, leche evaporada, crema de leche), con un toque de licor y canela, decorada con merengue italiano y cerezas",
-				imagen:"tresleche.jpg",
-				precio: 3800,
-			},
-
-			{
-				id: 2,
-				nombre: "Red Velvet",
-				desc: "Esta torta es para los que no son tan amantes del chocolate, su sabor es entre vainilla y chocolate suave, con triple relleno de crema de queso y cobertura de buttercream con chocolate blanco",
-				imagen:"redvelvet.jpg",
-				precio: 7000,
-			},
-
-			{
-				id: 3,
-				nombre: "Triple Chocolate",
-				desc: "Torta húmeda de chocolate, triple relleno de ganache de chocolate, cobertura de buttercream de chocolate semiamargo, drip y ferreros",
-				imagen:"triplechocolate.jpg",
-				precio: 7800,
-			},
-
-			{
-				id: 4,
-				nombre: "Matilda",
-				desc: "Torta de chocolate húmeda con triple relleno y cobertura de ganache de chocolate semiamargo",
-				imagen:"matilda.jpg",
-				precio: 7500,
-			},
-		];
-
 		const getProductos = new Promise((resolve) => {
 			setTimeout(() => {
-				resolve(productos);
+				resolve(Products);
 			}, 2000);
 		});
 
@@ -55,6 +20,7 @@ const ItemListContainer = (props) => {
 
 	return (
 		<div className="container">
+			<h1>{props.name}</h1>
 			<ItemList items={items} />
 		</div>
 	);
---
+++ web-app/src/components/mock/Products.jsx
@@ -0,0 +1,34 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Tres Leche",
+        desc: "Bizcochuelo húmedo en tres leches (leche condensada, leche evaporada, crema de leche), con un toque de licor y canela, decorada con merengue italiano y cerezas",
+        imagen:"tresleche.jpg",
+        precio: 3800
+    },
+
+    {
+        id: 2,
+        nombre: "Red Velvet",
+        desc: "Esta torta es para los que no son tan amantes del chocolate, su sabor es entre vainilla y chocolate suave, con triple relleno de crema de queso y cobertura de buttercream con chocolate blanco",
+        imagen:"redvelvet.jpg",
+        precio: 7000
+    },
+
+    {
+        id: 3,
+        nombre: "Triple Chocolate",
+        desc: "Torta húmeda de chocolate, triple relleno de ganache de chocolate, cobertura de buttercream de chocolate semiamargo, drip y ferreros",
+        imagen:"triplechocolate.jpg",
+        precio: 7800
+    },
+
+    {
+        id: 4,
+        nombre: "Matilda",
+        desc: "Torta de chocolate húmeda con triple relleno y cobertura de ganache de chocolate semiamargo",
+        imagen:"matilda.jpg",
+        precio: 7500
+    }
+];
+export default productos;
